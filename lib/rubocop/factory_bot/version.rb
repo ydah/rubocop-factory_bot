d49ccbf411d8d1ae052f83aa2eaf00451daf975b
ydah <13041216+ydah@users.noreply.github.com>
2023-02-21 22:46:42 +0900

    Change the release version

--- lib/rubocop/factory_bot/version.rb
+++ lib/rubocop/factory_bot/version.rb
@@ -4,7 +4,7 @@ module RuboCop
   module FactoryBot
     # Version information for the FactoryBot RuboCop plugin.
     module Version
-      STRING = '2.19.0'
+      STRING = '2.20.0'
     end
   end
 end
